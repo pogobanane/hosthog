7964c6a27417faa99f8757eb20b235d64733b387
Pogobanane <aenderboy@gmx.de>
2022-11-15 14:44:30 +0100

    post subcommand: do wall

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Args, Parser, Subcommand};
+use std::process::Command;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -39,9 +40,27 @@ enum Commands {
         #[command(flatten)]
         status: StatusCommand,
     },
+    /// post a message to all logged in users
+    ///
+    /// The message will arrive at:
+    /// - login shells (wall)
+    /// - all tmux sessions (tmux display-popup)
+    Post {
+        /// message to post
+        message: Vec<String>
+    },
+    /// List all logged in users
+    ///
+    /// Checks:
+    /// - login shells/ssh sessions (users)
+    /// - tmux sessions of all users
+    /// - xrdp sessions
+    /// - vscode?
+    Users {
+    }
 }
 
-fn show_status(cmd: &StatusCommand) {
+fn show_status(cmd: StatusCommand) {
     println!("Showing status.");
     println!("status list: {}", cmd.list);
 }
@@ -55,9 +74,35 @@ fn prog() -> Option<String> {
         .into()
 }
 
+/// successfully runs a command or crashes
+fn run(command: &Vec<String>) {
+    if command.len() < 1 {
+        panic!("can not execute emptystring");
+    }
+    let mut args = command.into_iter();
+    let bin = args.next().expect("can not execute emptystring");
+    let out = Command::new(bin)
+        .args(args)
+        .output()
+        .expect("failed to run binary");
+    if !out.status.success() {
+        panic!("{} exited with code {}", bin, out.status.code().expect("no exit code found"));
+    }
+    println!("{:?}", out.stdout);
+    println!("{:?}", out.stderr);
+}
+
+fn do_post(mut message: Vec<String>) {
+    println!("post message:");
+    message.as_slice().into_iter().for_each(|i| print!("{} ", i));
+    println!("");
+    message.insert(0, String::from("wall"));
+    run(&message);
+}
+
 fn main() {
     let cli = Cli::parse();
-    match &cli.command {
+    match cli.command {
         Some(Commands::Status { status }) => {
             show_status(status);
         }
@@ -67,13 +112,15 @@ fn main() {
         Some(Commands::Release { status }) => {
             println!("release list: {}", status.list);
         }
+        Some(Commands::Post{ message }) => do_post(message),
         None => {
             println!("print some global settings like link to calendar, spreadsheet or database");
-            show_status(&StatusCommand::default());
+            show_status(StatusCommand::default());
             println!(
                 "See more options with: {} help",
                 prog().expect("Your binary name looks very unexpected.")
             );
         }
+        _ => unimplemented!()
     };
 }
