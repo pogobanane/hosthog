79612583508f15e77e4c499de3735fd51de11f44
Pogobanane <aenderboy@gmx.de>
2023-10-11 23:11:27 +0200

    hog: bind mount /dev/null over all authorized_keys_files

--- src/hog.rs
+++ src/hog.rs
@@ -1,7 +1,68 @@
 use nix;
+use crate::diskstate;
 
-pub fn hog_ssh(users: Vec<String>) {
-    nix::mount::mount(Some("/home/peter/dev/phd/hosthog/foo"), "/home/peter/dev/phd/hosthog/bar", None::<&str>, nix::mount::MsFlags::MS_BIND, None::<&str>).expect("bind mount failed");
+fn overmount(file: &str) -> Result<(), ()> {
+    if !std::path::Path::new(file).is_file() {
+        println!("warn: overmount: file does not exist: {}", file);
+        return Err(());
+    }
+    println!("overmount: {}", file);
+    nix::mount::mount(
+        Some("/dev/null"), 
+        file,
+        None::<&str>, 
+        nix::mount::MsFlags::MS_BIND, 
+        None::<&str>
+    ).expect("bind mount failed");
+
+    println!("ok");
+    return Ok(());
+}
+
+pub struct User {
+    pub name: String,
+    pub home: String,
+}
+
+fn list_users() -> Vec<User> {
+    let mut users = vec![];
+
+    loop {
+        // safe because we null check before accessing it
+        let passwd = unsafe {
+            let passwd = libc::getpwent();
+            if passwd.is_null() { break };
+            *passwd
+        };
+        if passwd.pw_dir.is_null() { continue };
+        // safe because we null check before accessing it
+        let home = unsafe { std::ffi::CStr::from_ptr(passwd.pw_dir).to_string_lossy().into_owned() };
+        // safe because we null check before accessing it
+        if passwd.pw_name.is_null() { continue };
+        let name = unsafe { std::ffi::CStr::from_ptr(passwd.pw_name).to_string_lossy().into_owned() };
+
+        users.push(User { name, home });
+    }
+
+    // safe because i dont know what might be unsafe about it
+    unsafe { libc::endpwent() };
+
+    return users;
+}
+
+pub fn hog_ssh(exclude_users: Vec<String>, state: &mut diskstate::DiskState) {
+    let users = list_users().into_iter().filter(|u| !exclude_users.contains(&u.name)).collect::<Vec<_>>();
+    let auth_key_files = diskstate::expand_authorized_keys_file(&state.settings, users);
+    println!("overmounts: {:?}", auth_key_files);
+    for file in auth_key_files {
+        match overmount(&file) {
+            Ok(_) => { println!("ok"); state.overmounts.push(file); },
+            Err(_) => { },
+        }
+    }
+    println!("overmounts: {:?}", state.overmounts);
+
+
 
     // let mut command = vec![String::from("pkill"), String::from("-u")];
     // command.extend(users);
