bcf96a4a4f984ac5e49533d6b84f8fe9116b2bfd
Pogobanane <aenderboy@gmx.de>
2023-12-26 20:29:01 +0000

    users: mention when root is missing

--- src/users.rs
+++ src/users.rs
@@ -8,42 +8,33 @@ pub fn do_list_users() {
     println!("{}", String::from_utf8_lossy(&who.stdout));
     println!("");
 
-    println!("SSH sessions:");
-    let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
-    let proto_flags = ProtocolFlags::TCP | ProtocolFlags::UDP;
-    let sockets_info = get_sockets_info(af_flags, proto_flags).unwrap();
-    for si in sockets_info {
-        match si.protocol_socket_info {
-            ProtocolSocketInfo::Tcp(TcpSocketInfo {
-                state: TcpState::Established,
-                local_port: 22,
-                ..
-            }) => {
-                let mut cmdlines = vec![];
-                for pid in &si.associated_pids {
-                    let cmdline = std::fs::read_to_string(format!("/proc/{}/cmdline", pid)).unwrap();
-                    // trim trailing null bytes
-                    let cmdline = cmdline.trim_matches(char::from(0)).to_string();
-                    cmdlines.push(cmdline);
-                }
-                let cmdline = cmdlines.join(", ");
-                println!("{}", cmdline);
-                // println!("{:?}", si.associated_pids);
-            },
-            // ProtocolSocketInfo::Tcp(tcp_si) => println!(
-            //     "TCP {}:{} -> {}:{} {:?} - {}",
-            //     tcp_si.local_addr,
-            //     tcp_si.local_port,
-            //     tcp_si.remote_addr,
-            //     tcp_si.remote_port,
-            //     si.associated_pids,
-            //     tcp_si.state
-            // ),
-            // ProtocolSocketInfo::Udp(udp_si) => println!(
-            //     "UDP {}:{} -> *:* {:?}",
-            //     udp_si.local_addr, udp_si.local_port, si.associated_pids
-            // ),
-            _ => (),
+    if !is_root() {
+        println!("Skipping ssh sessions (you are not root)");
+    } else {
+        println!("SSH sessions:");
+        let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
+        let proto_flags = ProtocolFlags::TCP | ProtocolFlags::UDP;
+        let sockets_info = get_sockets_info(af_flags, proto_flags).unwrap();
+        for si in sockets_info {
+            match si.protocol_socket_info {
+                ProtocolSocketInfo::Tcp(TcpSocketInfo {
+                    state: TcpState::Established,
+                    local_port: 22,
+                    ..
+                }) => {
+                    let mut cmdlines = vec![];
+                    for pid in &si.associated_pids {
+                        let cmdline = std::fs::read_to_string(format!("/proc/{}/cmdline", pid)).unwrap();
+                        // trim trailing null bytes
+                        let cmdline = cmdline.trim_matches(char::from(0)).to_string();
+                        cmdlines.push(cmdline);
+                    }
+                    let cmdline = cmdlines.join(", ");
+                    println!("{}", cmdline);
+                    // println!("{:?}", si.associated_pids);
+                },
+                _ => (),
+            }
         }
     }
 
